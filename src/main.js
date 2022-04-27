b8a49f4ce998e990bff9440b3f7db882eab81178
enderson.villegas <ecvillegas@indracompany.com>
2022-04-26 23:21:43 -0500

    fix: main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const { casos } = require('../data/procesarDeducibles.data');
 const { procesarDeducibles } = require('./procesador');
 
+let i = 0;
 for (const caso of casos) {
   const { input, output } = caso;
   const resultado = procesarDeducibles(input);
-  console.log('validación', JSON.stringify(resultado) === JSON.stringify(output));
+  i++;
+  console.log(`validación caso ${i}:`, JSON.stringify(resultado) === JSON.stringify(output));
 }
