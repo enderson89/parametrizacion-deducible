9d1d5949fb4ea5c88e9988349be2da3f2cde5c46
enderson.villegas <ecvillegas@indracompany.com>
2022-04-26 23:21:05 -0500

    fix: procesador smells

--- src/procesador.js
+++ src/procesador.js
@@ -9,38 +9,31 @@ module.exports = {
     const tallerPorDefecto = 'NO TALLER';
     const deduciblePorDefecto = 0;
     const copagoPorDefecto = 0;
-  
+
     let text = input.payload.text;
     const txtTalleres = text.match(TALLER_REGEX)||[];
-    
+
     const payload = [];
     for (const txtTaller of txtTalleres) {
       const txtDeducible = txtTaller.match(/\d+(.\d+)?%/g)[0];
-      console.log('txtDeducible', txtDeducible);
       const deducible = txtDeducible ? parseInt(txtDeducible.replace('%', '')) : deduciblePorDefecto;
-  
+
       const txtCopago = txtTaller.match(/(US\$|S\/.)\s*\d+(.\d+)?/gi)[0];
-      console.log('txtCopago', txtCopago);
       const copago = txtCopago ? parseInt(txtCopago.replace(/(US\$|S\/.)\s*/gi, '')) : copagoPorDefecto;
-  
+
       const txtTipo = txtTaller.match(/\bmultimarca\b/gi);
-      console.log('txtTipo', txtTipo);
       const tipo = txtTipo ? txtTipo[0][0].toUpperCase() + txtTipo[0].slice(1) : tipoPorDefecto;
-  
+
       let taller = txtTaller.match(/(otro(s)? )?taller[\wá-ú\t, ]+(\d+(.\d+)?%|\n|$|\)|\.)/gi)[0];
-      console.log('taller1', taller);
       const palabraFiltroEncontrada = taller.search(/preferencia|especial|afiliado|otro/gi);
-      taller = palabraFiltroEncontrada == -1 ? taller.replace(/(\s*taller(es)?\s+|,\s+\d+(.\d+)?%)/gi, '').trimEnd() : tallerPorDefecto;
-      console.log('taller2', taller);
-  
+      taller = palabraFiltroEncontrada === -1 ? taller.replace(/(\s*taller(es)?\s+|,\s+\d+(.\d+)?%)/gi, '').trimEnd() : tallerPorDefecto;
+
       const txtMarca = txtTaller.match(/marca\s+[\wá-ú, ]+:/gi);
-      console.log('txtMarca', txtMarca);
       const marca = txtMarca ? txtMarca[0].replace(/(marca\s*|:)/gi, '').toUpperCase() : marcaPorDefecto;
-  
+
       const txtMoneda = txtTaller.match(/US\$|S\/./gi);
-      console.log('txtMoneda', txtMoneda);
       const moneda = txtMoneda ? txtMoneda[0].replace(/US\$/gi, monedaDolares).replace(/S\/./gi, monedaSoles).toUpperCase() : monedaDolares;
-  
+
       const deducibleParametrizado = {
         deducible,
         copago,
@@ -48,7 +41,7 @@ module.exports = {
         tipo,
         marca,
         taller
-      }
+      };
       payload.push(deducibleParametrizado);
     }
     return { payload };
